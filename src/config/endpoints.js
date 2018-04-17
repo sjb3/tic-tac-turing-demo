f5443641dc9dcfec3b41a8538f24ffaff60bd419
Sung J Byun <sungjbyun@D-69-91-185-148.dhcp4.washington.edu>
2018-04-16 21:16:32 -0700

    initial

---
+++ src/config/endpoints.js
@@ -0,0 +1,1 @@
+export const relayApi = 'https://api.graph.cool/relay/v1/cjg2wqbg874fw0178pae6ewc5'
---
+++ src/containers/Template.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+// import RaisedButton from 'material-ui/RaisedButton'
+import NavDrawer from '../components/NavDrawer';
+
+
+injectTapEventPlugin()
+
+class Template extends Component {
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <NavDrawer />
+          <main>
+            {this.props.children}
+          </main>
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+export default Template
